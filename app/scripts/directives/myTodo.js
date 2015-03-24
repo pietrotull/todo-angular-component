421dcbdd6913df381ab1902f989c841aae382a78
unknown <tgdkepi1@utgdkepi1-l1.corproot.net>
2015-03-25 00:07:40 +0100

    missing files

---
+++ app/scripts/directives/myTodo.js
@@ -0,0 +1,29 @@
+'use strict';
+angular.module('todoV2App')
+  .directive('myTodo', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      todo:'=',
+      index: '='
+    },
+    templateUrl: '/scripts/directives/myTodo.html',
+    controller: function ($scope, TodoService) {
+      $scope.markDone = function () {
+        TodoService.markDone($scope.index)
+      };
+
+      $scope.removeTodo = function (index) {
+        TodoService.removeTodo($scope.index)
+      };
+
+      $scope.isDone = function () {
+        if ($scope.todo.done) {
+          return 'todo-done';
+        } else {
+          return '';
+        }  
+      };
+    }
+  };
+});
